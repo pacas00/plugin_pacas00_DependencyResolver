04f83049d35902b03a8322780c3ff739828def58
Peter Cashel <pacas00@gmail.com>
2016-06-13 00:13:09 +1000

    inital commit

---
+++ Event/DependencyLogEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace net.PeterCashel.DependencyResolver
+{
+	public class DependencyLogEventArgs: EventArgs
+	{
+		public string LogMessage
+		{
+			private set; get;
+		}
+
+		public DependencyLogEventArgs(string Text)
+		{
+			LogMessage = Text;
+		}
+
+	}
+}
+
---
+++ plugin_pacas00_DependencyResolver.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace net.PeterCashel.DependencyResolver
+{
+	public class plugin_pacas00_DependencyResolver: FortressCraftMod
+	{
+		public plugin_pacas00_DependencyResolver()
+		{
+		}
+
+
+		public void Start()
+		{
+			string modName = "Pacas00's Dependency Resolver";
+			string modId = "pacas00.aaDependencyResolver";
+
+			string workingDir = "";
+
+			foreach(ModConfiguration current in ModManager.mModConfigurations.Mods)
+			{
+				if(current.Id.Contains(modId) || current.Name == modName)
+				{
+					workingDir = current.Path;
+					break;
+				}
+			}
+
+			DependencyResolver.logEvent += logHandler;
+
+
+			Directory.CreateDirectory(workingDir + Path.DirectorySeparatorChar + "lib");
+
+
+			DependencyResolver.Setup(workingDir + Path.DirectorySeparatorChar + "lib");
+
+
+			//if(DependencyResolver.isReadyForRegistration)
+			//{
+			//	//add libs
+			//}
+			//else {
+			//	DependencyResolver.registerEvent += registrationevent;
+			//	//add libs when ready.
+			//}
+
+		}
+
+
+		static void logHandler(object source, DependencyLogEventArgs e)
+		{
+			Debug.LogWarning(e.LogMessage);
+		}
+
+		//static void registrationevent(object source, EventArgs e)
+		//{
+		//	//Will be fired when ready
+		//}
+	}
+
+
+
+}
